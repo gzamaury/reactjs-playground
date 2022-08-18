836d700d2431e741a0129abe440b1cdc7ef4b3c9
Amaury Gz <gzamaury@hotmail.com>
2022-08-18 22:26:04 +0000

    Add: useMyOwnHook - Building Your Own Hooks

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import StateHook from "./examples/hooks/useState/StateHook";
 import EffectHook from "./examples/hooks/useEffect/EffectHook";
+import MyOwnHook, { MyOwnHook2 } from "./examples/hooks/useMyOwnHook/MyOwnHook";
 
 function App() {
   return (
     <div className="App">
       <StateHook />
       <EffectHook />
+      <MyOwnHook />
+      <MyOwnHook2 />
     </div>
   );
 }
